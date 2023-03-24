07d1d27388ab234d0562874f7691e930042c9869
Fedor <fedorveridian@gmail.com>
2023-03-25 00:28:55 +0300

    update

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readLineSync from 'readline-sync';
+
+const greeting = () => {
+const userName = readLineSync.question('May I have your name?');
+console.log(`Hello, ${userName}!`);
+};
+export default greeting;
